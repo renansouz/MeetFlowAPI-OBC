7ea27d6c9ef529b86b745578d0b827feb832db97
WesleyR10 <wesleyribas2015@gmail.com>
2024-03-19 23:04:52 -0300

    Creating useCase addUser

---
+++ src/slices/user/useCases/addUser/AddUser.ts
@@ -0,0 +1,16 @@
+import { UserData,UserEntity } from "@/slices/user/entities";
+import { AddUserRepository } from "@/slices/user/repositories";
+
+export type AddUser = (data: UserData) => Promise<UserEntity | null>;
+
+export type AddUserSignature = (
+  addUser: AddUserRepository,
+) => AddUser;
+
+    export class AddUserClass{
+      constructor(private addUserRepository: AddUserRepository){}
+
+      async execute(data: UserData): Promise<UserEntity | null> {
+        return this.addUserRepository.addUser(new UserEntity(data));
+      }
+    }
